77be8e35e974fe82f3ed407eecb29610c7106db0
ManePerez <s15120001@alumnos.itsur.edu.mx>
2020-03-12 10:54:57 -0600

    Actualizacion en el codigo para que funcione con cualquier version de android.

--- app/src/main/java/com/josemanuelperezparamo/mibroadcastreceiver/MiBroadcastReceiver.java
+++ app/src/main/java/com/josemanuelperezparamo/mibroadcastreceiver/MiBroadcastReceiver.java
@@ -1,40 +1,54 @@
 package com.josemanuelperezparamo.mibroadcastreceiver;
 
+import android.Manifest;
+import android.app.Service;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
+import android.content.pm.PackageManager;
 import android.icu.text.UnicodeSetSpanner;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
+import android.telephony.PhoneStateListener;
 import android.telephony.SmsManager;
 import android.telephony.TelephonyManager;
+import android.util.Log;
 import android.widget.Toast;
+
+import androidx.core.app.ActivityCompat;
 
 public class MiBroadcastReceiver extends BroadcastReceiver {
 
     @Override
-    public void onReceive(Context context, Intent intent) {
-        Bundle extras = intent.getExtras();
-        if(extras!=null){
-            if(extras.getString("state").equals(TelephonyManager.EXTRA_STATE_RINGING)){
-                String numero = intent.getExtras().getString(TelephonyManager.EXTRA_INCOMING_NUMBER);
-                String nume=MainActivity.in.getStringExtra("tel");
-                String text=MainActivity.in.getStringExtra("texto");
+    public void onReceive(final Context context, Intent intent) {
+        final String tel = MainActivity.in.getStringExtra("tel");
+        final String texto = MainActivity.in.getStringExtra("texto");
 
-                //Toast.makeText(context, "Te esta llamando: " + numero+" numero a bloquear: "+nume, Toast.LENGTH_LONG).show();
-                if(numero.equals("+52"+nume)){
-                    Toast.makeText(context, "SON IGUALES", Toast.LENGTH_LONG).show();
-                    SmsManager sms = SmsManager.getDefault();
-                    sms.sendTextMessage(nume, null, text , null, null);
-                }else if(numero.equals(nume)){
-                    Toast.makeText(context, "SON IGUALES", Toast.LENGTH_LONG).show();
-                    SmsManager sms = SmsManager.getDefault();
-                    sms.sendTextMessage(nume, null, text , null, null);
-                }else{
-                    Toast.makeText(context, "diferentes", Toast.LENGTH_LONG).show();
+        TelephonyManager telephonyManager = (TelephonyManager) context.getSystemService(
+                Service.TELEPHONY_SERVICE
+        );
+
+        telephonyManager.listen(new PhoneStateListener() {
+            @Override
+            public void onCallStateChanged(int state, String phoneNumber) {
+                super.onCallStateChanged(state, phoneNumber);
+                if (state == TelephonyManager.CALL_STATE_RINGING) {
+                    if (phoneNumber.equals("+52" + tel)) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(tel, null, texto, null, null);
+                        Toast.makeText(context, "Mensaje envíado.", Toast.LENGTH_LONG).show();
+                    } else if (phoneNumber.equals(tel)) {
+                        SmsManager smsManager = SmsManager.getDefault();
+                        smsManager.sendTextMessage(tel, null, texto, null, null);
+                        Toast.makeText(context, "Mensaje envíado.", Toast.LENGTH_LONG).show();
+                    } else {
+                        Toast.makeText(context, "El número ingresado: " +
+                                tel + "\nes diferente del número entrante: " +
+                                phoneNumber, Toast.LENGTH_LONG).show();
+                    }
                 }
             }
-        }
+        }, PhoneStateListener.LISTEN_CALL_STATE);
     }
 }
